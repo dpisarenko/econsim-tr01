1f79bca0494d2f59f46ae53d02560d1ec3f397c3
Dmitri Pisarenko <dp@altruix.co>
2016-04-02 15:55:05 +0300

    Working on a simple agricultural simulation.

---
+++ src/test/java/cc/altruix/econsimtr01/SimulationTests.kt
@@ -0,0 +1,15 @@
+package cc.altruix.econsimtr01
+
+import org.junit.Test
+
+/**
+ * @author Dmitri Pisarenko (dp@altruix.co)
+ * @version $Id$
+ * @since 1.0
+ */
+class SimulationTests {
+    @Test
+    fun test() {
+
+    }
+}
