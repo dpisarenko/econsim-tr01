c76c19506650c1e40799c26de8b597ef055dcd30
Dmitri Pisarenko <dp@altruix.co>
2016-04-10 19:23:34 +0300

    Added flows for the first steady state (C. Speich).

--- src/test/resources/ch0201Sim1Tests.params.pl
+++ src/test/resources/ch0201Sim1Tests.params.pl
@@ -1,7 +1,12 @@
 agent(stacy).
 agent(employer).
+agent(landlord).
+agent(groceryStore).
+agent(savingsAccount).
 resource(r1, "Source code modification", "Hits of code").
 resource(r2, "Money", "2016 US dollars").
+resource(r3, "Accomodation", "Days the person is allowed to live in the flat").
+resource(r4, "Food", "Calories").
 hasFlow(f1,
     stacy,
     employer,
@@ -15,3 +20,42 @@ hasFlow(f2,
     3000.0,
     oncePerMonth(30)
 ).
+% Rent
+hasFlow(f3,
+    stacy,
+    landlord,
+    r2,
+    1000.0,
+    oncePerMonth(1)
+).
+hasFlow(f4,
+    landlord,
+    stacy,
+    r3,
+    30.0,
+    oncePerMonth(1)
+).
+% Food
+hasFlow(f5,
+    stacy,
+    groceryStore,
+    r2,
+    30.0,
+    daily(19, 00)
+).
+hasFlow(f5,
+    stacy,
+    groceryStore,
+    r4,
+    2534.0,
+    daily(19, 01)
+).
+% Savings
+hasFlow(f6,
+    stacy,
+    savingsAccount,
+    r2,
+    1000.0,
+    oncePerMonth(1)
+).
+
