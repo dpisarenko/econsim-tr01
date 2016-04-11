a05672f46b4acb4da1630d66f1b68a700ca3879d
Dmitri Pisarenko <dp@altruix.co>
2016-04-11 23:00:22 +0300

    Working on cc.altruix.econsimtr01.ch0201.Sim1TimeSeriesCreator.

--- src/main/java/cc/altruix/econsimtr01/ch0201/Sim1TimeSeriesCreator.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/Sim1TimeSeriesCreator.kt
@@ -1,6 +1,11 @@
 package cc.altruix.econsimtr01.ch0201
 
+import alice.tuprolog.Prolog
+import cc.altruix.econsimtr01.newLine
+import cc.altruix.econsimtr01.removeSingleQuotes
+import cc.altruix.javaprologinterop.PlUtils
 import java.io.File
+import java.util.*
 
 /**
  * @author Dmitri Pisarenko (dp@altruix.co)
@@ -16,7 +21,35 @@ class Sim1TimeSeriesCreator {
                 "Money @ Stacy",
                 "Money in savings account")
 
+        val prolog = PlUtils.createEngine()
+        PlUtils.loadPrologFiles(
+                prolog,
+                arrayOf(input.absolutePath)
+        )
+        val times = PlUtils.getResults(prolog,
+                "measurementTime(Time, _).",
+                "Time").map { x -> x.toLong() }.toList()
+        Collections.sort(times)
+
+        times.forEach { t ->
+            val timeShort = t.toString()
+            val timeLong = extractTimeLongForm(prolog, t)
+            val moneyAtStacy = "" // TODO: Continue here
+            val moneyInSavingsAccount = "" // TODO: Continue here
+
+            appendRow(builder,
+                    timeShort,
+                    timeLong,
+                    moneyAtStacy,
+                    moneyInSavingsAccount)
+        }
+        // TODO: Continue here, integrate this class into simulation 1
         return builder.toString()
+    }
+    private fun extractTimeLongForm(prolog: Prolog, t: Long): String {
+        return PlUtils
+                .extractSingleStringFromQuery(prolog, "measurementTime($t, X).", "X")
+                .removeSingleQuotes()
     }
 
     private fun appendRow(builder: StringBuilder,
@@ -24,6 +57,12 @@ class Sim1TimeSeriesCreator {
                           timeLong: String,
                           moneyAtStacy: String,
                           moneyInSavingsAccount: String) {
-
+        arrayOf(timeShort,
+                timeLong,
+                moneyAtStacy,
+                moneyInSavingsAccount).forEach { col ->
+            builder.append("\"$col\";")
+        }
+        builder.newLine()
     }
 }
