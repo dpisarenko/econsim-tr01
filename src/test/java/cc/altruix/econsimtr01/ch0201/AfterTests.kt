b62a66aebe2b3a864f2d80ae008a4664f5e16be7
pisarenko <dp@altruix.co>
2016-04-14 13:36:36 +0300

    Added test cc.altruix.econsimtr01.ch0201.AfterTests.updateNextFiringTimeSunnyDay.

--- src/test/java/cc/altruix/econsimtr01/ch0201/AfterTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/AfterTests.kt
@@ -1,5 +1,7 @@
 package cc.altruix.econsimtr01.ch0201
 
+import cc.altruix.econsimtr01.millisToSimulationDateTime
+import cc.altruix.econsimtr01.secondsToSimulationDateTime
 import cc.altruix.econsimtr01.shouldBe
 import org.junit.Test
 
@@ -11,5 +13,13 @@ class AfterTests {
     fun initResetsNextFireTime() {
         val out = After("f1")
         out.nextFireTime.shouldBe(-1)
+    }
+
+    @Test
+    fun updateNextFiringTimeSunnyDay() {
+        val out = After("f1")
+        val t = 0L.millisToSimulationDateTime()
+        out.updateNextFiringTime(t)
+        out.nextFireTime.shouldBe(t.millis + 1L)
     }
 }
