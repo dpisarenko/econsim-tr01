bbc14969c4ea4331aabd912e67f5f1802f3365ea
Dmitri Pisarenko <dp@altruix.co>
2016-04-17 15:24:34 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0201/Sim2Accountant.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/Sim2Accountant.kt
@@ -7,7 +7,7 @@ import cc.altruix.econsimtr01.PlResource
 /**
  * Created by pisarenko on 14.04.2016.
  */
-class Sim2Accountant(logTarget: StringBuilder,
+open class Sim2Accountant(logTarget: StringBuilder,
                      agents: List<IAgent>,
                      resources: List<PlResource>) : AbstractAccountant(logTarget, agents, resources) {
     companion object {
@@ -22,12 +22,11 @@ class Sim2Accountant(logTarget: StringBuilder,
     }
 
     override fun logStockLevels(time: Long) {
-        // TODO: Test this
         logNormalStockLevels(time)
         logCohortData(time)
     }
 
-    internal fun logNormalStockLevels(time: Long) {
+    open internal fun logNormalStockLevels(time: Long) {
         agents.forEach { agent ->
             resources
                     .filter { !cohortResources.contains(it.id) }
@@ -35,7 +34,7 @@ class Sim2Accountant(logTarget: StringBuilder,
         }
     }
 
-    internal fun logCohortData(time: Long) {
+    open internal fun logCohortData(time: Long) {
         // TODO: Implement this
         // TODO: Test this
         throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
