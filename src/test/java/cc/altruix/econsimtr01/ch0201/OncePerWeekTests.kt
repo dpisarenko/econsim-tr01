c4b48335670c5f7dedd3a698f53d51b863c92808
Dmitri Pisarenko <dp@altruix.co>
2016-04-17 23:03:52 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/OncePerWeekTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/OncePerWeekTests.kt
@@ -8,7 +8,7 @@ import org.junit.Test
  * Created by pisarenko on 14.04.2016.
  */
 class OncePerWeekTests {
-    // TODO: Continue here
+    // TODO: Continue here, Once per week must fire every Monday, at Midnight
     @Test
     fun sunnyDay() {
         val daysOfWeek = arrayOf("Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
