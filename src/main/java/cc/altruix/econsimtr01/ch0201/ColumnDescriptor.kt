96530694be48834c6a514d01440c5168671eb91e
Dmitri Pisarenko <dp@altruix.co>
2016-04-17 11:25:57 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0201/ColumnDescriptor.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/ColumnDescriptor.kt
@@ -1,9 +1,11 @@
 package cc.altruix.econsimtr01.ch0201
 
+import alice.tuprolog.Prolog
+
 /**
  * @author Dmitri Pisarenko (dp@altruix.co)
  * @version $Id$
  * @since 1.0
  */
-class ColumnDescriptor {
-}
+data class ColumnDescriptor(val title:String,
+                            val func: (Prolog, Long) -> String)
