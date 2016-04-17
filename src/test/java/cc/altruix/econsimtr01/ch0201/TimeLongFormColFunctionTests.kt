65ab299a7b946e223586df764dc38c4a7f751dad
Dmitri Pisarenko <dp@altruix.co>
2016-04-17 14:41:18 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/TimeLongFormColFunctionTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/TimeLongFormColFunctionTests.kt
@@ -1,9 +1,19 @@
 package cc.altruix.econsimtr01.ch0201
 
+import cc.altruix.econsimtr01.shouldBe
+import cc.altruix.econsimtr01.toPrologTheory
+import org.junit.Test
+
 /**
  * @author Dmitri Pisarenko (dp@altruix.co)
  * @version $Id$
  * @since 1.0
  */
 class TimeLongFormColFunctionTests {
+    @Test
+    fun invoke() {
+        val prolog = "measurementTime(172800, '0000-01-03 00:00').".toPrologTheory()
+        val act = TimeLongFormColFunction().invoke(prolog, 172800L)
+        act.shouldBe("0000-01-03 00:00")
+    }
 }
