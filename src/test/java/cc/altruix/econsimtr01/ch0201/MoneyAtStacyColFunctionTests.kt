4bdb2f578223b0e82132c91683b45ce23eecfa61
Dmitri Pisarenko <dp@altruix.co>
2016-04-17 14:47:56 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/MoneyAtStacyColFunctionTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/MoneyAtStacyColFunctionTests.kt
@@ -1,5 +1,7 @@
 package cc.altruix.econsimtr01.ch0201
 
+import cc.altruix.econsimtr01.shouldBe
+import cc.altruix.econsimtr01.toPrologTheory
 import org.junit.Test
 
 /**
@@ -10,6 +12,9 @@ import org.junit.Test
 class MoneyAtStacyColFunctionTests {
     @Test
     fun invoke() {
-
+        val out = MoneyAtStacyColFunction()
+        val prolog = "resourceLevel(172800, 'stacy', 'r2', 3000.0).".toPrologTheory()
+        val act = out.invoke(prolog, 172800L)
+        act.shouldBe("3000.0")
     }
 }
