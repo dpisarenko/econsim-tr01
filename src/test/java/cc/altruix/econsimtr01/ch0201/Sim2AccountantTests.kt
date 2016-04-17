683d6488b8d48a34425d54987490f9cea5157087
Dmitri Pisarenko <dp@altruix.co>
2016-04-17 16:04:46 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/Sim2AccountantTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/Sim2AccountantTests.kt
@@ -2,6 +2,8 @@ package cc.altruix.econsimtr01.ch0201
 
 import cc.altruix.econsimtr01.IAgent
 import cc.altruix.econsimtr01.PlResource
+import cc.altruix.econsimtr01.shouldBe
+import org.fest.assertions.Assertions
 import org.junit.Test
 import org.mockito.Mockito
 
@@ -13,12 +15,7 @@ import org.mockito.Mockito
 class Sim2AccountantTests {
     @Test
     fun logStockLevelsCallsLogCohortData() {
-        val logTarget = StringBuilder()
-        val agents = emptyList<IAgent>()
-        val resources = emptyList<PlResource>()
-        val out = Mockito.spy(Sim2Accountant(logTarget,
-                agents,
-                resources))
+        val out = createSim2Accountant()
         Mockito.doNothing().`when`(out).logNormalStockLevels(1L)
         Mockito.doNothing().`when`(out).logCohortData(1L)
         // Run method under test
@@ -26,5 +23,30 @@ class Sim2AccountantTests {
         // Verify
         Mockito.verify(out).logNormalStockLevels(1L)
         Mockito.verify(out).logCohortData(1L)
+    }
+
+    protected fun createSim2Accountant(): Sim2Accountant {
+        val logTarget = StringBuilder()
+        val agents = emptyList<IAgent>()
+        val resources = emptyList<PlResource>()
+        val out = Mockito.spy(Sim2Accountant(logTarget,
+                agents,
+                resources))
+        return out
+    }
+
+    @Test
+    fun createSubscribersCountByNumberOfInteractions() {
+        val out = createSim2Accountant()
+        val act = out.createSubscribersCountByNumberOfInteractions()
+        act.size.shouldBe(7)
+        Assertions.assertThat(act.get(0)).isNull()
+        Assertions.assertThat(act.get(1)?.get()).isEqualTo(0)
+        Assertions.assertThat(act.get(2)?.get()).isEqualTo(0)
+        Assertions.assertThat(act.get(3)?.get()).isEqualTo(0)
+        Assertions.assertThat(act.get(4)?.get()).isEqualTo(0)
+        Assertions.assertThat(act.get(5)?.get()).isEqualTo(0)
+        Assertions.assertThat(act.get(6)?.get()).isEqualTo(0)
+        Assertions.assertThat(act.get(7)?.get()).isEqualTo(0)
     }
 }
