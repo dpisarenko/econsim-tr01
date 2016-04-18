abbb6fffffe9e1bd922a1273bc2a1615a09c5d3b
pisarenko <dp@altruix.co>
2016-04-18 21:25:19 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/Sim3Tests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/Sim3Tests.kt
@@ -1,6 +1,7 @@
 package cc.altruix.econsimtr01.ch0201
 
 import cc.altruix.econsimtr01.ResourceFlow
+import cc.altruix.econsimtr01.shouldBe
 import cc.altruix.econsimtr01.simulationRunLogic
 import org.junit.Test
 import java.io.File
@@ -33,5 +34,12 @@ class Sim3Tests {
                 "src/test/resources/ch0201/sim03/Sim3Tests.test.flows.actual.png",
                 Sim2TimeSeriesCreator()
         )
+    }
+    @Test
+    fun flowF8IsCreated() {
+        val simParametersProvider = Sim3ParametersProvider(
+                File("src/test/resources/ch0201Sim3Tests.params.pl").readText()
+        )
+        simParametersProvider.flows.filter { it.id == "f8" }.count().shouldBe(1)
     }
 }
