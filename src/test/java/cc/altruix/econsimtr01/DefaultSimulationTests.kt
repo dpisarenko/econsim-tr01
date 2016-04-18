24f7a0346b5f65a33d78609ef070d7930d0e35dc
pisarenko <dp@altruix.co>
2016-04-18 13:38:10 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/DefaultSimulationTests.kt
+++ src/test/java/cc/altruix/econsimtr01/DefaultSimulationTests.kt
@@ -1,13 +1,47 @@
 package cc.altruix.econsimtr01
 
+import cc.altruix.econsimtr01.ch0201.InfiniteResourceSupply
+import cc.altruix.econsimtr01.ch0201.InitialResourceLevel
+import org.joda.time.DateTime
 import org.junit.Ignore
 import org.junit.Test
+import java.util.*
 
 /**
  * Created by pisarenko on 18.04.2016.
  */
 class DefaultSimulationTests {
-    //class DefaultSimulationForTesting : DefaultSimulation(Timing())
+    class TickCollectingAgent : IAgent {
+        val times = LinkedList<DateTime>()
+        override fun act(time: DateTime) {
+            times.add(time)
+        }
+
+        override fun id(): String = "Jimmy"
+
+    }
+    class SimParametersProvider(override val agents: MutableList<IAgent>,
+                                override val flows: MutableList<PlFlow>,
+                                override val initialResourceLevels: MutableList<InitialResourceLevel>,
+                                override val infiniteResourceSupplies: MutableList<InfiniteResourceSupply>) :
+            ISimParametersProvider {
+
+    }
+    class DefaultSimulationForTesting : DefaultSimulation(
+            SimParametersProvider(
+                    LinkedList<IAgent>(),
+                    LinkedList<PlFlow>(),
+                    LinkedList<InitialResourceLevel>(),
+                    LinkedList<InfiniteResourceSupply>()
+            )
+    ) {
+        override fun continueCondition(tick: DateTime): Boolean = true
+
+        override fun createAgents(): List<IAgent> = listOf(TickCollectingAgent())
+
+        override fun createSensors(): List<ISensor> = emptyList()
+
+    }
     @Test
     @Ignore
     fun runTicksEveryMinute() {
