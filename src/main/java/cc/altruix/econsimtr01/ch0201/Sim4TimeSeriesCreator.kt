fd186f23a943b74789a6389d72ba97ab962df014
pisarenko <dp@altruix.co>
2016-04-19 08:13:19 +0300

    Working.

---
+++ src/main/java/cc/altruix/econsimtr01/ch0201/Sim4TimeSeriesCreator.kt
@@ -0,0 +1,15 @@
+package cc.altruix.econsimtr01.ch0201
+
+import cc.altruix.econsimtr01.DefaultTimeSeriesCreator
+import java.io.File
+
+/**
+ * Created by pisarenko on 19.04.2016.
+ */
+class Sim4TimeSeriesCreator : DefaultTimeSeriesCreator() {
+    override fun prologToCsv(input: File): String {
+        // TODO: Implement this
+        // TODO: Test this
+        throw UnsupportedOperationException()
+    }
+}
