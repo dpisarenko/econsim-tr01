bab91b1947e9b32a4e6464c6e4571077487b01b4
pisarenko <dp@altruix.co>
2016-04-19 16:50:59 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/PlTransformationTests.kt
+++ src/test/java/cc/altruix/econsimtr01/PlTransformationTests.kt
@@ -2,6 +2,7 @@ package cc.altruix.econsimtr01
 
 import org.joda.time.DateTime
 import org.junit.Test
+import org.mockito.Mockito
 
 /**
  * Created by pisarenko on 19.04.2016.
@@ -9,6 +10,13 @@ import org.junit.Test
 class PlTransformationTests {
     @Test
     fun timeToRunCallsTimeTriggerFunction() {
+        val out = createPlTransformation()
+        out.timeToRun(DateTime(2016, 4, 19, 16, 45, 0)).shouldBeTrue()
+        out.timeToRun(DateTime(2016, 4, 19, 16, 45, 0).plusMinutes(1)).shouldBeFalse()
+        out.timeToRun(DateTime(2016, 4, 19, 16, 45, 0).minusMinutes(1)).shouldBeFalse()
+    }
+
+    private fun createPlTransformation(): PlTransformation {
         val out = PlTransformation(
                 "id",
                 "agent",
@@ -16,11 +24,20 @@ class PlTransformationTests {
                 "res1",
                 10.0,
                 "res2",
-                 {
-            x -> (x.hourOfDay == 16) && (x.minuteOfHour == 45) && (x.secondOfMinute == 0)
-        })
-        out.timeToRun(DateTime(2016, 4, 19, 16, 45, 0)).shouldBeTrue()
-        out.timeToRun(DateTime(2016, 4, 19, 16, 45, 0).plusMinutes(1)).shouldBeFalse()
-        out.timeToRun(DateTime(2016, 4, 19, 16, 45, 0).minusMinutes(1)).shouldBeFalse()
+                {
+                    x ->
+                    (x.hourOfDay == 16) && (x.minuteOfHour == 45) && (x.secondOfMinute == 0)
+                })
+        return out
+    }
+
+    @Test
+    fun subscriptionWorks() {
+        val out = createPlTransformation()
+        val subscriber = mock<IActionSubscriber>()
+        out.subscribe(subscriber)
+        val time = 0L.millisToSimulationDateTime().plusMinutes(123)
+        out.notifySubscribers(time)
+        Mockito.verify(subscriber).actionOccurred(out, time)
     }
 }
