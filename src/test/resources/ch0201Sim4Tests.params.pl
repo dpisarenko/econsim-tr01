b1ca2a2f55927f4e2a42bca8b3ee2ba8e409f362
pisarenko <dp@altruix.co>
2016-04-19 12:54:08 +0300

    Working.

--- src/test/resources/ch0201Sim4Tests.params.pl
+++ src/test/resources/ch0201Sim4Tests.params.pl
@@ -7,6 +7,9 @@ resource(r1, "Source code modification", "Hits of code").
 resource(r2, "Money", "2016 US dollars").
 resource(r3, "Accomodation", "Days the person is allowed to live in the flat").
 resource(r4, "Food", "Calories").
+resource("r06-pc1", "People, who were exposed to Stacy''s writings once", "People").
+resource(r13, "Time available for business-building", "Hours").
+
 hasFlow(f1,
     stacy,
     employer,
