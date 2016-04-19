79f15ccd8754b79116603429230129ba71ac8464
pisarenko <dp@altruix.co>
2016-04-19 21:02:37 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/Sim4Tests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/Sim4Tests.kt
@@ -1,7 +1,10 @@
 package cc.altruix.econsimtr01.ch0201
 
 import cc.altruix.econsimtr01.ResourceFlow
+import cc.altruix.econsimtr01.shouldBeFalse
+import cc.altruix.econsimtr01.shouldBeTrue
 import cc.altruix.econsimtr01.simulationRunLogic
+import org.joda.time.DateTime
 import org.junit.Test
 import java.io.File
 import java.util.*
@@ -35,5 +38,19 @@ class Sim4Tests {
                 Sim4TimeSeriesCreator()
         )
     }
-
+    @Test
+    fun continueCondition() {
+        val flows = LinkedList<ResourceFlow>()
+        val log = StringBuilder()
+        val simParametersProvider = Sim4ParametersProvider(
+                File("src/test/resources/ch0201Sim4Tests.params.pl").readText()
+        )
+        val sim = Sim4(
+                log,
+                flows,
+                simParametersProvider
+        )
+        sim.continueCondition(DateTime(0, 1, 1, 0, 0, 0)).shouldBeTrue()
+        sim.continueCondition(DateTime(1, 1, 1, 0, 0, 0)).shouldBeFalse()
+    }
 }
