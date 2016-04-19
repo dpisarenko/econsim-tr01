ade6d55600f2371744423398d8f28671f885757b
pisarenko <dp@altruix.co>
2016-04-19 09:03:21 +0300

    Added stuff from steady-state A.

--- src/test/java/cc/altruix/econsimtr01/ch0201/Sim4Tests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/Sim4Tests.kt
@@ -12,7 +12,10 @@ import java.util.*
 class Sim4Tests {
     @Test
     fun test() {
-        // TODO: Stacy already puts 500 dollars per month into a savings account.
+        // TODO: Add a resource "productive time".
+        // TODO: Make sure that at the beginning of every week that productive time is set to 40 hours.
+        // TODO: Flow, every week. 20 hours of time are transformed into imaginary software features. By working 20 hours per week, she adds X percen to software completion.
+
         // TODO: 1. Stacy has done this for a while and knows that if she invests 20 hours per week over 6 months, the software will be completed. In other words, it costs her 480 person hours (20 hours per week * 4 weeks per month * 6 months = 480) to develop the software.
         // TODO: 1. If Stacy gets rid of distractions, she can spend up to 40 hours per week on productive activities, which aren't related to her day job.
         // TODO: 1. Stacy spends 20 hours per week on the guest posting activities (incl. all preparatory steps) and publishes 1 guest post every two weeks.
