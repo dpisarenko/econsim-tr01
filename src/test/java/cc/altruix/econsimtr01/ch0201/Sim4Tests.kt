83ee12e9c4b1889142cbbd1c726c505418d8167f
pisarenko <dp@altruix.co>
2016-04-19 13:16:56 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/Sim4Tests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/Sim4Tests.kt
@@ -12,10 +12,6 @@ import java.util.*
 class Sim4Tests {
     @Test
     fun test() {
-        // TODO: Flow, every week. 20 hours of time are transformed into imaginary software features. By working 20 hours per week, she adds X percen to software completion.
-
-        // TODO: 1. Stacy has done this for a while and knows that if she invests 20 hours per week over 6 months, the software will be completed. In other words, it costs her 480 person hours (20 hours per week * 4 weeks per month * 6 months = 480) to develop the software.
-        // TODO: 1. If Stacy gets rid of distractions, she can spend up to 40 hours per week on productive activities, which aren't related to her day job.
         // TODO: 1. Stacy spends 20 hours per week on the guest posting activities (incl. all preparatory steps) and publishes 1 guest post every two weeks.
         // TODO: 1. 1 guest post brings her, on average, 20 new subscribers.
 
