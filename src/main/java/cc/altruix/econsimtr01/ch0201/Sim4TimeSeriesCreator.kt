04028006c32bec84f73c2fac98496ac1c3562e44
pisarenko <dp@altruix.co>
2016-04-20 08:27:40 +0300

    Added TODOs for the lunch session.

--- src/main/java/cc/altruix/econsimtr01/ch0201/Sim4TimeSeriesCreator.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/Sim4TimeSeriesCreator.kt
@@ -7,6 +7,11 @@ import java.io.File
  * Created by pisarenko on 19.04.2016.
  */
 class Sim4TimeSeriesCreator : DefaultTimeSeriesCreator() {
+    // TODO: Add column: "Time short form"
+    // TODO: Add column: "Time long form"
+    // TODO: Add column: "Amount in savings account"
+    // TODO: Add column: "People in the list"
+    // TODO: Add column: "Software completion (percentage of 480 hours)"
     override fun prologToCsv(input: File): String {
         // TODO: Implement this
         // TODO: Test this
