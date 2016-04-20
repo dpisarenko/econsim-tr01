b9593af93d77ccb5d767a7a513cebcbc81f30828
pisarenko <dp@altruix.co>
2016-04-20 07:45:03 +0300

    Working.

---
+++ src/test/java/cc/altruix/econsimtr01/ch0201/WhenResourceReachesLevelTests.kt
@@ -0,0 +1,23 @@
+package cc.altruix.econsimtr01.ch0201
+
+import cc.altruix.econsimtr01.DefaultAgent
+import cc.altruix.econsimtr01.mock
+import org.junit.Test
+import org.mockito.Mockito
+
+/**
+ * Created by pisarenko on 20.04.2016.
+ */
+class WhenResourceReachesLevelTests {
+    @Test
+    fun connectToInitiatingAgent() {
+        val out = WhenResourceReachesLevel("agent", "resource", 1.0)
+        val agent = mock<DefaultAgent>()
+        Mockito.`when`(agent.id).thenReturn("agent")
+        // Run method under test
+        out.connectToInitiatingAgent(listOf(agent))
+        // Verify
+        Mockito.verify(agent).addResourceLevelObserver(out)
+    }
+
+}
