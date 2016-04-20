be8310c1d88d8f253dbcd4e949de9209164a5ceb
pisarenko <dp@altruix.co>
2016-04-20 07:39:56 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0201/WhenResourceReachesLevel.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/WhenResourceReachesLevel.kt
@@ -1,6 +1,8 @@
 package cc.altruix.econsimtr01.ch0201
 
+import cc.altruix.econsimtr01.DefaultAgent
 import cc.altruix.econsimtr01.IAgent
+import cc.altruix.econsimtr01.IResourceLevelObserver
 import cc.altruix.econsimtr01.findAgent
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
@@ -10,7 +12,9 @@ import org.slf4j.LoggerFactory
  */
 open class WhenResourceReachesLevel(val agent:String,
                                val resource:String,
-                               val amount:Double) : (DateTime) -> Boolean {
+                               val amount:Double) : (DateTime) -> Boolean,
+        IResourceLevelObserver {
+
     val LOGGER = LoggerFactory.getLogger(WhenResourceReachesLevel::class.java)
     override fun invoke(time: DateTime): Boolean {
         // TODO: Implement this
@@ -25,5 +29,13 @@ open class WhenResourceReachesLevel(val agent:String,
             LOGGER.error("Can't find agent '$agent'")
             return
         }
+        if (agent is DefaultAgent) {
+            agent.addResourceLevelObserver(this)
+        }
+    }
+    override fun possibleResourceLevelChange(agent: DefaultAgent, time: DateTime) {
+        // TODO: Implement this
+        // TODO: Test this
+
     }
 }
