72489b2aece3f3acc29033f5415f9e3264b6acec
pisarenko <dp@altruix.co>
2016-04-21 09:47:50 +0300

    Working.

--- src/test/java/cc/altruix/econsimtr01/ch0201/TransitionReadinessColFunctionTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0201/TransitionReadinessColFunctionTests.kt
@@ -1,5 +1,6 @@
 package cc.altruix.econsimtr01.ch0201
 
+import cc.altruix.econsimtr01.shouldBe
 import cc.altruix.econsimtr01.shouldBeFalse
 import cc.altruix.econsimtr01.shouldBeTrue
 import cc.altruix.econsimtr01.toPrologTheory
@@ -38,5 +39,39 @@ class TransitionReadinessColFunctionTests {
         out.enoughMoney("resourceLevel(86400, savingsAccount, r2, 2999.0).".toPrologTheory(), 86400L).shouldBeFalse()
         out.enoughMoney("resourceLevel(86400, savingsAccount, r2, 3000.0).".toPrologTheory(), 86400L).shouldBeTrue()
         out.enoughMoney("resourceLevel(86400, savingsAccount, r2, 3000.1).".toPrologTheory(), 86400L).shouldBeTrue()
+    }
+    @Test
+    fun invoke() {
+        val out = TransitionReadinessColFunction()
+        val th1 =
+        """
+        resourceLevel(86400, 'stacy', 'r14', 480.0).
+        resourceLevel(86400, list, 'r06-pc1', 1000.0).
+        resourceLevel(86400, savingsAccount, r2, 3000.0).
+        """
+        val th2 =
+        """
+        resourceLevel(86400, 'stacy', 'r14', 479.0).
+        resourceLevel(86400, list, 'r06-pc1', 1000.0).
+        resourceLevel(86400, savingsAccount, r2, 3000.0).
+        """
+        val th3 =
+                """
+        resourceLevel(86400, 'stacy', 'r14', 480.0).
+        resourceLevel(86400, list, 'r06-pc1', 999.0).
+        resourceLevel(86400, savingsAccount, r2, 3000.0).
+        """
+        val th4 =
+                """
+        resourceLevel(86400, 'stacy', 'r14', 480.0).
+        resourceLevel(86400, list, 'r06-pc1', 1000.0).
+        resourceLevel(86400, savingsAccount, r2, 2999.99).
+        """
+
+        out.invoke(th1.toPrologTheory(), 86400L).shouldBe("yes")
+
+        arrayOf(th2, th3, th4)
+                .map {it.toPrologTheory()}
+                .forEach { out.invoke(it, 86400L).shouldBe("no") }
     }
 }
