5eb89adfaa436da2d1c4d4ee643ef3987f6e4bbd
pisarenko <dp@altruix.co>
2016-04-21 14:03:32 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0201/Sim4ParametersProvider.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/Sim4ParametersProvider.kt
@@ -1,8 +1,15 @@
 package cc.altruix.econsimtr01.ch0201
 
+import alice.tuprolog.Prolog
+import alice.tuprolog.SolveInfo
+import cc.altruix.econsimtr01.PlFlow
+
 /**
  * Created by pisarenko on 19.04.2016.
  */
 open class Sim4ParametersProvider(theoryTxt2:String) :
         Sim2ParametersProvider(theoryTxt2) {
+    override fun createFlow(res: SolveInfo, prolog: Prolog): PlFlow =
+            super.createFlow(extractFlowData(res))
+
 }
