14afa3dd5490b590c2451344083f4b6f4198fa76
pisarenko <dp@altruix.co>
2016-04-21 14:10:37 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0201/Sim4ParametersProvider.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0201/Sim4ParametersProvider.kt
@@ -2,7 +2,7 @@ package cc.altruix.econsimtr01.ch0201
 
 import alice.tuprolog.Prolog
 import alice.tuprolog.SolveInfo
-import cc.altruix.econsimtr01.PlFlow
+import cc.altruix.econsimtr01.*
 
 /**
  * Created by pisarenko on 19.04.2016.
@@ -11,5 +11,12 @@ open class Sim4ParametersProvider(theoryTxt2:String) :
         Sim2ParametersProvider(theoryTxt2) {
     override fun createFlow(res: SolveInfo, prolog: Prolog): PlFlow =
             super.createFlow(extractFlowData(res))
+    override fun readAgents(prolog: Prolog) {
+        val agentsPl = prolog.getResults("isAgent(X).", "X")
+        agentsPl
+                .map { x -> x.removeSingleQuotes() }
+                .map { DefaultAgent(it) }
+                .forEach { this.agents.add(it) }
+    }
 
 }
