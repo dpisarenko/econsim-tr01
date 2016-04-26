dc933aba2a5eb9b9b3f1fe844937dca09bdeff10
pisarenko <dp@altruix.co>
2016-04-26 13:10:04 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0202/Sim1.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/Sim1.kt
@@ -1,7 +1,32 @@
 package cc.altruix.econsimtr01.ch0202
 
+import cc.altruix.econsimtr01.DefaultSimulation
+import cc.altruix.econsimtr01.IAgent
+import cc.altruix.econsimtr01.ISensor
+import cc.altruix.econsimtr01.ResourceFlow
+import org.joda.time.DateTime
+
 /**
  * Created by pisarenko on 26.04.2016.
  */
-class Sim1 {
+class Sim1(val logTarget:StringBuilder,
+           val flows:MutableList<ResourceFlow>,
+           simParametersProvider: Sim1ParametersProvider) : DefaultSimulation(simParametersProvider) {
+    override fun continueCondition(tick: DateTime): Boolean {
+        // TODO: Implement this
+        // TODO: Test this
+        throw UnsupportedOperationException()
+    }
+
+    override fun createAgents(): List<IAgent> {
+        // TODO: Implement this
+        // TODO: Test this
+        throw UnsupportedOperationException()
+    }
+
+    override fun createSensors(): List<ISensor> {
+        // TODO: Implement this
+        // TODO: Test this
+        throw UnsupportedOperationException()
+    }
 }
