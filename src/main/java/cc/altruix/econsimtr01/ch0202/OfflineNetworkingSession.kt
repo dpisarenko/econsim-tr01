31473f7578b51b9d3ccb52bc762afebdcf54fdb7
pisarenko <dp@altruix.co>
2016-04-27 14:03:09 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSession.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSession.kt
@@ -1,13 +1,32 @@
 package cc.altruix.econsimtr01.ch0202
 
 import cc.altruix.econsimtr01.DefaultAction
+import cc.altruix.econsimtr01.IActionSubscriber
 import org.joda.time.DateTime
 
 /**
  * Created by pisarenko on 27.04.2016.
  */
-// TODO: Fix this mess
-class OfflineNetworkingSession(val offlineNetworkingIntensity:Int) :
-        DefaultAction(this)  {
+class OfflineNetworkingSession(val offlineNetworkingIntensity:Int,
+                               val agent:Protagonist) :
+        DefaultAction(
+                OfflineNetworkingSessionTriggerFun(
+                        offlineNetworkingIntensity,
+                        agent
+                )
+        )  {
+    override fun run(time: DateTime) {
+
+        throw UnsupportedOperationException()
+    }
+
+    override fun notifySubscribers(time: DateTime) {
+        throw UnsupportedOperationException()
+    }
+
+    override fun subscribe(subscriber: IActionSubscriber) {
+        throw UnsupportedOperationException()
+    }
+
     override fun timeToRun(time: DateTime): Boolean = false
 }
--- src/main/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSessionTriggerFun.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSessionTriggerFun.kt
@@ -5,7 +5,8 @@ import org.joda.time.DateTime
 /**
  * Created by pisarenko on 27.04.2016.
  */
-class OfflineNetworkingSessionTriggerFun : (DateTime) -> Boolean {
+class OfflineNetworkingSessionTriggerFun(val offlineNetworkingIntensity:Int,
+                                         val agent:Protagonist) : (DateTime) -> Boolean {
     override fun invoke(p1: DateTime): Boolean {
         // TODO: Implement this
         // TODO: Test this
