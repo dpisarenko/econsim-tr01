eae482d5a91e463cf1dbd3426fe045e069516307
pisarenko <dp@altruix.co>
2016-04-27 13:50:30 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0202/Protagonist.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/Protagonist.kt
@@ -1,18 +1,38 @@
 package cc.altruix.econsimtr01.ch0202
 
 import cc.altruix.econsimtr01.DefaultAgent
+import cc.altruix.econsimtr01.PlFlow
+import cc.altruix.econsimtr01.ch0201.OncePerWeek
 import org.joda.time.DateTime
 
 /**
  * Created by pisarenko on 26.04.2016.
  */
-class Protagonist(val offlineNetworkingIntensity:Int) : DefaultAgent("protagonist") {
+class Protagonist(val offlineNetworkingIntensity:Int,
+                  val availableTimePerWeek:Int) : DefaultAgent(ID) {
+    companion object {
+        val ID = "protagonist"
+    }
+    init {
+        this.addAction(
+                PlFlow(
+                        id = "prF1",
+                        src = "nature",
+                        target = ID,
+                        resource = Sim1.RESOURCE_AVAILABLE_TIME.id,
+                        amount = availableTimePerWeek.toDouble(),
+                        timeTriggerFunction = OncePerWeek("Monday")
+                )
+        )
+    }
     /**
      * TODO: Remove these notes
      *
      * Where are we stuck?
      *
      * TODO: Add a flow, which adds X hours of available time to Protagonist
+     *
+     * How is it done in the previous simulation?
      */
     override fun act(time: DateTime) {
         super.act(time)
--- src/main/java/cc/altruix/econsimtr01/ch0202/Sim1.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/Sim1.kt
@@ -1,15 +1,17 @@
 package cc.altruix.econsimtr01.ch0202
 
-import cc.altruix.econsimtr01.DefaultSimulation
-import cc.altruix.econsimtr01.IAgent
-import cc.altruix.econsimtr01.ISensor
-import cc.altruix.econsimtr01.ResourceFlow
+import cc.altruix.econsimtr01.*
 import org.joda.time.DateTime
 
 class Sim1(val logTarget:StringBuilder,
            val flows:MutableList<ResourceFlow>,
            simParametersProvider: Sim1ParametersProvider) : DefaultSimulation(simParametersProvider) {
     companion object {
+        val RESOURCE_AVAILABLE_TIME = PlResource(
+                id = "r1",
+                name = "Available time",
+                unit = "Hours"
+        )
         /**
          * OFFLINE_NETWORKING_INTENSITY
          * Unit: People per week
@@ -32,7 +34,12 @@ class Sim1(val logTarget:StringBuilder,
 
     override fun createAgents(): List<IAgent> {
         // TODO: Test this
-        return listOf(Protagonist(OFFLINE_NETWORKING_INTENSITY))
+        return listOf(
+                Protagonist(
+                        OFFLINE_NETWORKING_INTENSITY,
+                        AVAILABLE_TIME_PER_WEEK
+                )
+        )
     }
 
     override fun createSensors(): List<ISensor> {
