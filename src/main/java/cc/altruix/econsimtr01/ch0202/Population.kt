a48a7716238045055511cd08dcdfcc8bcf2f62ac
Dmitri Pisarenko <dp@altruix.co>
2016-05-02 16:22:01 +0300

    Working.

--- src/main/java/cc/altruix/econsimtr01/ch0202/Population.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/Population.kt
@@ -1,7 +1,14 @@
 package cc.altruix.econsimtr01.ch0202
 
+import java.util.*
+
 /**
  * Created by pisarenko on 27.04.2016.
  */
-class Population : IPopulation {
+class Population(val initialNetworkSize:Int) : IPopulation {
+    val people:MutableList<Person> = LinkedList()
+    init {
+        // TODO: Implement this
+        // TODO: Test this
+    }
 }
