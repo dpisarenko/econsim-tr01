6b20ab78b22173cd5b5b2b9336e375c537ad2ca3
Dmitri Pisarenko <dp@altruix.co>
2016-05-03 19:35:29 +0300

    Implemented test cc.altruix.econsimtr01.ch0202.OfflineNetworkingSessionTests.validateDetectsAgentHasTooLittleTimeLeft.

--- src/test/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSessionTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSessionTests.kt
@@ -16,6 +16,12 @@ class OfflineNetworkingSessionTests {
 
     @Test
     fun validateDetectsDailyLimitExceeded() {
+        dailyLimitDetectionTestLogic(3)
+        dailyLimitDetectionTestLogic(4)
+        dailyLimitDetectionTestLogic(5)
+    }
+
+    private fun dailyLimitDetectionTestLogic(sessionsAlreadyHeldToday: Int) {
         val population = Population(100)
         val agent = Protagonist(
                 availableTimePerWeek = 40,
@@ -27,13 +33,41 @@ class OfflineNetworkingSessionTests {
                 maxNetworkingSessionsPerBusinessDay = 3,
                 timePerOfflineNetworkingSession = 3.0,
                 population = population)
-        agent.offlineNetworkingSessionsHeldDuringCurrentDay = 3
+        agent.offlineNetworkingSessionsHeldDuringCurrentDay = sessionsAlreadyHeldToday
         Assertions.assertThat(out.validate()).isFalse()
     }
+
     @Test
     fun validateDetectsAgentHasTooLittleTimeLeft() {
-        // TODO: Implement this
+        agentHasTooLittleTimeLeftTestLogic(1.0)
+        agentHasTooLittleTimeLeftTestLogic(2.0)
+        agentHasTooLittleTimeLeftTestLogic(2.99)
     }
+
+    private fun agentHasTooLittleTimeLeftTestLogic(availableTime: Double) {
+        val population = Population(100)
+        val agent = Protagonist(
+                availableTimePerWeek = 40,
+                maxNetworkingSessionsPerBusinessDay = 2,
+                timePerOfflineNetworkingSessions = 3.0,
+                population = population
+        )
+        agent.offlineNetworkingSessionsHeldDuringCurrentDay = 2
+        agent.put(
+                Sim1.RESOURCE_AVAILABLE_TIME.id,
+                availableTime
+        )
+        Assertions
+                .assertThat(agent.amount(Sim1.RESOURCE_AVAILABLE_TIME.id))
+                .isEqualTo(availableTime)
+        val out = OfflineNetworkingSession(agent = agent,
+                maxNetworkingSessionsPerBusinessDay = 3,
+                timePerOfflineNetworkingSession = 3.0,
+                population = population
+        )
+        Assertions.assertThat(out.validate()).isFalse()
+    }
+
     @Test
     fun validateReturnsTrueIfEverythingOk() {
         // TODO: Implement this
