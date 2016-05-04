dd70908c26e5014deccf9bd5a1ab49a79f120774
pisarenko <dp@altruix.co>
2016-05-04 08:05:53 +0300

    Test runs.

---
+++ src/test/java/cc/altruix/econsimtr01/ch0202/OfflineNetworkingSessionsHeldDuringDayResetActionTests.kt
@@ -0,0 +1,32 @@
+package cc.altruix.econsimtr01.ch0202
+
+import cc.altruix.econsimtr01.millisToSimulationDateTime
+import cc.altruix.econsimtr01.mock
+import org.fest.assertions.Assertions
+import org.junit.Test
+
+/**
+ * Created by pisarenko on 04.05.2016.
+ */
+class OfflineNetworkingSessionsHeldDuringDayResetActionTests {
+    @Test
+    fun run() {
+        // Prepare
+        val agent = Protagonist(
+                availableTimePerWeek = 40,
+                maxNetworkingSessionsPerBusinessDay = 2,
+                timePerOfflineNetworkingSessions = 3.0,
+                recommendationConversion = Sim1.RECOMMENDATION_CONVERSION,
+                willingnessToPurchaseConversion = Sim1.WILLINGNESS_TO_PURCHASE_CONVERSION,
+                population = mock<IPopulation>())
+        val out = OfflineNetworkingSessionsHeldDuringDayResetAction(
+                agent,
+                {true}
+        )
+        agent.offlineNetworkingSessionsHeldDuringCurrentDay = 42
+        // Run method under test
+        out.run(0L.millisToSimulationDateTime())
+        // Verify
+        Assertions.assertThat(agent.offlineNetworkingSessionsHeldDuringCurrentDay).isEqualTo(0)
+    }
+}
