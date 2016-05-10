c8fc10b6a74bdaef5ccb6a6746af716090707c1f
Dmitri Pisarenko <dp@altruix.co>
2016-05-10 23:15:37 +0300

    Trying to fix hat bug (lateinit property agents not initialized).

--- src/main/java/cc/altruix/econsimtr01/ch0202/Sim1a.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/Sim1a.kt
@@ -1,11 +1,9 @@
 package cc.altruix.econsimtr01.ch0202
 
-import cc.altruix.econsimtr01.IAction
-import cc.altruix.econsimtr01.IAgent
-import cc.altruix.econsimtr01.ISensor
-import cc.altruix.econsimtr01.ResourceFlow
+import cc.altruix.econsimtr01.*
 import cc.altruix.econsimtr01.ch0201.OncePerWeek
 import org.joda.time.DateTime
+import java.util.*
 
 /**
  * Created by pisarenko on 04.05.2016.
@@ -17,11 +15,19 @@ class Sim1a(logTarget:StringBuilder,
         Sim1(logTarget, flows, simParametersProvider) {
     override fun createAgents(): List<IAgent> {
         val agents = super.createAgents()
+        val flows = LinkedList<PlFlow>()
+        agents.forEach { agt ->
+            if (agt is DefaultAgent) {
+                agt.actions
+                        .filter { it is PlFlow }
+                        .forEach { flows.add(it as PlFlow) }
+            }
+        }
         attachFlowsToAgents(
-                (simParametersProvider as Sim1ParametersProvider).flows,
-                simParametersProvider.agents,
+                flows,
+                ArrayList(agents),
                 this.flows)
-
+        // TODO: Add agent nature
         return agents
     }
 
