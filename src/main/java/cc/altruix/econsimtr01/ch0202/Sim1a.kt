1137f08867700df197340769019124e70d04f9c5
pisarenko <dp@altruix.co>
2016-05-10 15:08:55 +0300

    Attempt at bugfix:
    kotlin.UninitializedPropertyAccessException: lateinit property agents has not been initialized
    
    	at cc.altruix.econsimtr01.PlFlow.run(PlFlow.kt:31)
    	at cc.altruix.econsimtr01.PlFlow.run(PlFlow.kt:27)
    	at cc.altruix.econsimtr01.DefaultAgent.act(DefaultAgent.kt:15)
    	at cc.altruix.econsimtr01.ch0202.Protagonist.act(Protagonist.kt:55)
    	at cc.altruix.econsimtr01.DefaultSimulation.minimalSimulationCycle$econsim_tr01(DefaultSimulation.kt:27)
    	at cc.altruix.econsimtr01.DefaultSimulation.run(DefaultSimulation.kt:17)
    	at cc.altruix.econsimtr01.ch0202.Sim1aTests.test(Sim1aTests.kt:48)
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
    	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
    	at java.lang.reflect.Method.invoke(Method.java:483)
    	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:44)
    	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:15)
    	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:41)
    	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:20)
    	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:274)
    	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:70)
    	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:48)
    	at org.junit.runners.ParentRunner$3.run(ParentRunner.java:242)
    	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:58)
    	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:240)
    	at org.junit.runners.ParentRunner.access$000(ParentRunner.java:48)
    	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:233)
    	at org.junit.runners.ParentRunner.run(ParentRunner.java:303)
    	at org.junit.runner.JUnitCore.run(JUnitCore.java:157)
    	at com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:119)
    	at com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:42)
    	at com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:234)
    	at com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:74)
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
    	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
    	at java.lang.reflect.Method.invoke(Method.java:483)
    	at com.intellij.rt.execution.application.AppMain.main(AppMain.java:144)

--- src/main/java/cc/altruix/econsimtr01/ch0202/Sim1a.kt
+++ src/main/java/cc/altruix/econsimtr01/ch0202/Sim1a.kt
@@ -1,6 +1,7 @@
 package cc.altruix.econsimtr01.ch0202
 
 import cc.altruix.econsimtr01.IAction
+import cc.altruix.econsimtr01.IAgent
 import cc.altruix.econsimtr01.ISensor
 import cc.altruix.econsimtr01.ResourceFlow
 import cc.altruix.econsimtr01.ch0201.OncePerWeek
@@ -14,6 +15,16 @@ class Sim1a(logTarget:StringBuilder,
             simParametersProvider: Sim1ParametersProvider,
             val resultsStorage:MutableMap<DateTime,Sim1aResultsRow>) :
         Sim1(logTarget, flows, simParametersProvider) {
+    override fun createAgents(): List<IAgent> {
+        val agents = super.createAgents()
+        attachFlowsToAgents(
+                (simParametersProvider as cc.altruix.econsimtr01.ch0201.Sim1ParametersProvider).flows,
+                simParametersProvider.agents,
+                this.flows)
+
+        return agents
+    }
+
     override fun createUnattachedProcesses(): List<IAction> {
         val sim1Params = simParametersProvider as Sim1ParametersProvider
         return listOf(
