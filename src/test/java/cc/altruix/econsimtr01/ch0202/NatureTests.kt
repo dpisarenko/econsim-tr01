68c6fbf8934b745679f83c1f3ef51072b80367b4
pisarenko <dp@altruix.co>
2016-05-11 17:28:49 +0300

    Added test cc.altruix.econsimtr01.ch0202.NatureTests.initSetsInfiniteResourceLevel.

---
+++ src/test/java/cc/altruix/econsimtr01/ch0202/NatureTests.kt
@@ -0,0 +1,15 @@
+package cc.altruix.econsimtr01.ch0202
+
+import org.fest.assertions.Assertions
+import org.junit.Test
+
+/**
+ * Created by pisarenko on 11.05.2016.
+ */
+class NatureTests {
+    @Test
+    fun initSetsInfiniteResourceLevel() {
+        val out = Nature()
+        Assertions.assertThat(out.isInfinite(Sim1ParametersProvider.RESOURCE_AVAILABLE_TIME.id))
+    }
+}
