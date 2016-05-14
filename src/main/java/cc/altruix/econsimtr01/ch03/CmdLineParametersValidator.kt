99f0c2c1cf40ccb9b61470d6df9f1f088d0c89e0
pisarenko <dp@altruix.co>
2016-05-14 18:26:54 +0300

    Working on the agricultural simulation.

--- src/main/java/cc/altruix/econsimtr01/ch03/CmdLineParametersValidator.kt
+++ src/main/java/cc/altruix/econsimtr01/ch03/CmdLineParametersValidator.kt
@@ -8,6 +8,12 @@ class CmdLineParametersValidator : ICmdLineParametersValidator {
         val USAGE = "Usage: java -cp java -cp econsim-tr01-1.0-SNAPSHOT-jar-with-dependencies.jar cc.altruix.econsimtr01.ch03.BasicAgriculturalSimulationAppKt sim1.properties [sim2.properties...]"
     }
     override fun validate(args: Array<String>): CmdLineParametersValidationResult {
+        if (args.isEmpty()) {
+            return CmdLineParametersValidationResult(
+                    valid = false,
+                    message = USAGE
+            )
+        }
         // TODO: Test this
         // TODO: Implement this
         throw UnsupportedOperationException()
