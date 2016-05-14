a6bcb964627fd5473deeb91f8e98d0a346a5f8d9
pisarenko <dp@altruix.co>
2016-05-14 15:24:43 +0300

    Added info on how to run the application.

---
+++ README.md
@@ -0,0 +1,9 @@
+Basic agriculture model
+=======================
+How to build
+------------
+`mvn assembly:single`
+
+How to run
+----------
+`java -cp econsim-tr01-1.0-SNAPSHOT-jar-with-dependencies.jar cc.altruix.econsimtr01.ch03.BasicAgriculturalSimulationAppKt`
---
+++ src/main/java/cc/altruix/econsimtr01/ch03/BasicAgriculturalSimulationApp.kt
@@ -0,0 +1,15 @@
+package cc.altruix.econsimtr01.ch03
+
+/**
+ * Created by pisarenko on 13.05.2016.
+ */
+class BasicAgriculturalSimulationApp {
+    fun main(args : Array<String>) {
+        println("Basic agriculture simulation")
+        println("(C) Copyright 2016 Dmitri Pisarenko")
+    }
+}
+fun main(args : Array<String>) {
+    println("Basic agriculture simulation")
+    println("(C) Copyright 2016 Dmitri Pisarenko")
+}
