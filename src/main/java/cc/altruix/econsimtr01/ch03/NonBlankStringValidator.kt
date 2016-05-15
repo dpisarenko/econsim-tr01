29b0e0a82616855c8f9b8f5609f85bb2bb63e909
pisarenko <dp@altruix.co>
2016-05-15 16:19:37 +0300

    Tests run.

--- src/main/java/cc/altruix/econsimtr01/ch03/NonBlankStringValidator.kt
+++ src/main/java/cc/altruix/econsimtr01/ch03/NonBlankStringValidator.kt
@@ -1,14 +1,17 @@
 package cc.altruix.econsimtr01.ch03
 
+import cc.altruix.econsimtr01.createCorrectValidationResult
+import cc.altruix.econsimtr01.createIncorrectValidationResult
+import org.apache.commons.lang3.StringUtils
 import java.util.*
 
 /**
  * Created by pisarenko on 14.05.2016.
  */
 object NonBlankStringValidator : IPropertiesFileValueValidator {
-    override fun validate(data: Properties, param: String): ValidationResult {
-        // TODO: Implement this
-        // TODO: Validate this
-        return ValidationResult(false, "")
-    }
+    override fun validate(data: Properties, param: String): ValidationResult =
+        when {
+            StringUtils.isBlank(data.getProperty(param)) -> createCorrectValidationResult()
+            else -> createIncorrectValidationResult("Value of parameter '$param' is blank")
+        }
 }
