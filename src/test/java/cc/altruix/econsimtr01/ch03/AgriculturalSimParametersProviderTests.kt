b673d1f2e58f9bebbdc33e689c710d74ee884aba
pisarenko <dp@altruix.co>
2016-05-15 19:37:31 +0300

    Implemented tests in cc.altruix.econsimtr01.ch03.AgriculturalSimParametersProviderTests.

--- src/test/java/cc/altruix/econsimtr01/ch03/AgriculturalSimParametersProviderTests.kt
+++ src/test/java/cc/altruix/econsimtr01/ch03/AgriculturalSimParametersProviderTests.kt
@@ -1,9 +1,28 @@
 package cc.altruix.econsimtr01.ch03
 
+import org.fest.assertions.Assertions
+import org.junit.Test
+import java.io.File
+import java.util.*
+
 /**
  * Created by pisarenko on 14.05.2016.
  */
 class AgriculturalSimParametersProviderTests {
-    // TODO: Start testing validation of data in properties files
-    // We have 2 validators - non-zero positive double and day of month validator
+    @Test
+    fun ryeSimulationParametersCorrect() {
+        simulationParametersCorrectnessTestLogic("BasicAgriculturalSimulationRye.properties")
+    }
+    @Test
+    fun wheatSimulationParametersCorrect() {
+        simulationParametersCorrectnessTestLogic("BasicAgriculturalSimulationWheat.properties")
+    }
+
+    private fun simulationParametersCorrectnessTestLogic(fileName: String) {
+        val out = AgriculturalSimParametersProvider(File("src/test/resources/ch03/$fileName"))
+        out.initAndValidate()
+        Assertions.assertThat(out.validity.valid).isTrue()
+        Assertions.assertThat(out.validity.message).isEmpty()
+// TODO: Test this
+    }
 }
