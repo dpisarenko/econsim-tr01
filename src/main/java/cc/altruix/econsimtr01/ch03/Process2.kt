d20a0f4b5e3e1d804f0924b8c7077574153af70a
pisarenko <dp@altruix.co>
2016-05-17 08:25:17 +0300

    Test runs.

--- src/main/java/cc/altruix/econsimtr01/ch03/Process2.kt
+++ src/main/java/cc/altruix/econsimtr01/ch03/Process2.kt
@@ -17,18 +17,11 @@ class Process2(val simParamProv:AgriculturalSimParametersProvider) : IAction {
             .parseDayMonthString()
     var field = simParamProv.agents.find { it.id() == Field.ID }
             as DefaultAgent
-
-    // TODO: Make sure this process converts RESOURCE_AREA_WITH_SEEDS to
-    // RESOURCE_AREA_WITH_CROP
-
     override fun timeToRun(time: DateTime): Boolean =
             time.isEqual(end.toDateTime(time.year)) &&
             evenHourAndMinute(time)
-
     open internal fun evenHourAndMinute(time: DateTime): Boolean = time
             .evenHourAndMinute(0, 0)
-
-    // TODO: Test this
     override fun run(time: DateTime) {
         val areaWithSeeds = field.amount(AgriculturalSimParametersProvider
                 .RESOURCE_AREA_WITH_SEEDS.id)
@@ -52,10 +45,8 @@ class Process2(val simParamProv:AgriculturalSimParametersProvider) : IAction {
                 areaWithSeeds * yieldPerSquareMeter
         )
     }
-
     override fun notifySubscribers(time: DateTime) {
     }
-
     override fun subscribe(subscriber: IActionSubscriber) {
     }
 }
