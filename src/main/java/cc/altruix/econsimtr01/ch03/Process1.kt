7f21eaf58626829bf3d52ab3edb47ae939e268d1
pisarenko <dp@altruix.co>
2016-05-17 07:13:40 +0300

    Implemented cc.altruix.econsimtr01.ch03.Process1.run.

--- src/main/java/cc/altruix/econsimtr01/ch03/Process1.kt
+++ src/main/java/cc/altruix/econsimtr01/ch03/Process1.kt
@@ -20,10 +20,17 @@ open class Process1(val simParamProv:PropertiesFileSimParametersProvider) :
             .parseDayMonthString()
     val field = simParamProv.agents.find { it.id() == Field.ID }
             as DefaultAgent
+    val shack = simParamProv.agents.find { it.id() == Shack.ID }
+            as DefaultAgent
+    // TODO: Test that businessDay is taken into account
     override fun timeToRun(time: DateTime): Boolean =
+            businessDay(time) &&
             timeBetweenStartAndEnd(time) &&
             evenHourAndMinute(time) &&
             fieldNotFull(field)
+
+    open internal fun businessDay(time: DateTime): Boolean =
+            time.isBusinessDay()
 
     open internal fun evenHourAndMinute(time:DateTime):Boolean =
             (time.hourOfDay == 8) && (time.minuteOfHour == 0)
@@ -32,15 +39,64 @@ open class Process1(val simParamProv:PropertiesFileSimParametersProvider) :
             time.between(start, end)
 
     open internal fun fieldNotFull(field: DefaultAgent): Boolean =
-            field.amount(
-                    AgriculturalSimParametersProvider
-                            .RESOURCE_AREA_WITH_SEEDS.id
-            ) < simParamProv.data["SizeOfField"].toString().toDouble()
+            fieldAreaWithSeeds(field) < sizeOfField()
+
+    internal fun sizeOfField() = simParamProv.data["SizeOfField"].toString()
+            .toDouble()
+
+    internal fun fieldAreaWithSeeds(field: DefaultAgent): Double {
+        return field.amount(
+                AgriculturalSimParametersProvider
+                        .RESOURCE_AREA_WITH_SEEDS.id
+        )
+    }
 
     override fun run(time: DateTime) {
         // TODO: Implement this
         // TODO: Test this
+        val workersCount = simParamProv.data["NumberOfWorkers"].toString()
+                .toDouble()
 
+        val laborPerDay = simParamProv.data["LaborPerBusinessDay"].toString()
+                .toDouble()
+        val effortPerSquareMeter =
+                simParamProv.data["Process1EffortInSquareMeters"].toString()
+                .toDouble()
+        val maxDailyProcessedArea = (workersCount * laborPerDay) /
+                effortPerSquareMeter
+        val totalAreaToPutSeedsIn = sizeOfField() - fieldAreaWithSeeds(field)
+
+        val dailyProcessedArea =
+                Math.min(maxDailyProcessedArea, totalAreaToPutSeedsIn)
+
+        if (field.amount(AgriculturalSimParametersProvider
+                .RESOURCE_EMPTY_AREA.id) < dailyProcessedArea) {
+            LOGGER.error("Not enough empty area on the field")
+            return
+        }
+
+        val seedsqPerSquareMeter = simParamProv
+                .data["Process1QuantityOfSeeds"].toString().toDouble()
+        val dailySeeds = dailyProcessedArea * seedsqPerSquareMeter
+
+        if (shack.amount(AgriculturalSimParametersProvider.RESOURCE_SEEDS.id)
+                < dailySeeds) {
+            LOGGER.error("Not enough empty seeds in the shack")
+            return
+
+        }
+        field.remove(
+                AgriculturalSimParametersProvider.RESOURCE_EMPTY_AREA.id,
+                dailyProcessedArea
+        )
+        field.put(
+                AgriculturalSimParametersProvider.RESOURCE_AREA_WITH_SEEDS.id,
+                dailyProcessedArea
+        )
+        shack.remove(
+                AgriculturalSimParametersProvider.RESOURCE_SEEDS.id,
+                dailySeeds
+        )
     }
     override fun notifySubscribers(time: DateTime) {
     }
