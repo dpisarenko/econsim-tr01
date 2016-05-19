0fe79f7c74e4eed864c5f2b9d81341beb36c1158
pisarenko <dp@altruix.co>
2016-05-19 08:14:15 +0300

    Updated the copyright.

--- src/main/java/cc/altruix/econsimtr01/ch03/BasicAgriculturalSimulationApp.kt
+++ src/main/java/cc/altruix/econsimtr01/ch03/BasicAgriculturalSimulationApp.kt
@@ -1,5 +1,14 @@
 /*
- * Copyright (c) 2016 Dmitri Pisarenko, http://altruix.cc
+ * Copyright 2012-2016 Dmitri Pisarenko
+ *
+ * WWW: http://altruix.cc
+ * E-Mail: dp@altruix.co
+ * Skype: dp118m (voice calls must be scheduled in advance)
+ * Physical address:
+ *
+ * 4-i Rostovskii pereulok 2/1/20
+ * 119121 Moscow
+ * Russian Federation
  */
 
 package cc.altruix.econsimtr01.ch03
--- src/test/java/cc/altruix/econsimtr01/ch03/AgriculturalSimParametersProviderWithPredefinedData.kt
+++ src/test/java/cc/altruix/econsimtr01/ch03/AgriculturalSimParametersProviderWithPredefinedData.kt
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2012-2016 Dmitri Pisarenko
+ *
+ * WWW: http://altruix.cc
+ * E-Mail: dp@altruix.co
+ * Skype: dp118m (voice calls must be scheduled in advance)
+ * Physical address:
+ *
+ * 4-i Rostovskii pereulok 2/1/20
+ * 119121 Moscow
+ * Russian Federation
+ */
+
 package cc.altruix.econsimtr01.ch03
 
 import java.io.File
