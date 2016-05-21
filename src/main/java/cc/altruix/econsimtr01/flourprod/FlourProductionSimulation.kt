92d2b644d6f1fc19c111a75b7664f015394ee82f
Dmitri Pisarenko <dp@altruix.co>
2016-05-21 12:44:50 +0300

    Creating skeletons of the flour production simulation classes.

---
+++ src/main/java/cc/altruix/econsimtr01/flourprod/FlourProductionSimulation.kt
@@ -0,0 +1,10 @@
+package cc.altruix.econsimtr01.flourprod
+
+/**
+ * @author Dmitri Pisarenko (dp@altruix.co)
+ * @version $Id$
+ * @since 1.0
+ */
+class FlourProductionSimulation {
+    // TODO: Implement this
+}
---
+++ src/main/java/cc/altruix/econsimtr01/flourprod/FlourProductionSimulationParametersProvider.kt
@@ -0,0 +1,10 @@
+package cc.altruix.econsimtr01.flourprod
+
+/**
+ * @author Dmitri Pisarenko (dp@altruix.co)
+ * @version $Id$
+ * @since 1.0
+ */
+class FlourProductionSimulationParametersProvider {
+    // TODO: Implement this
+}
