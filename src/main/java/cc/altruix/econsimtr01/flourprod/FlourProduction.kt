dae1df84be6003b4ead295a45c3ffbf198407ffa
Dmitri Pisarenko <dp@altruix.co>
2016-05-22 16:05:43 +0300

    Started to work on cc.altruix.econsimtr01.flourprod.FlourProduction.

---
+++ src/main/java/cc/altruix/econsimtr01/flourprod/FlourProduction.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012-2016 Dmitri Pisarenko
+ *
+ * WWW: http://altruix.cc
+ * E-Mail: dp@altruix.co
+ * Skype: dp118m (voice calls must be scheduled in advance)
+ *
+ * Physical address:
+ *
+ * 4-i Rostovskii pereulok 2/1/20
+ * 119121 Moscow
+ * Russian Federation
+ *
+ * This file is part of econsim-tr01.
+ *
+ * econsim-tr01 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * econsim-tr01 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with econsim-tr01.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package cc.altruix.econsimtr01.flourprod
+
+import cc.altruix.econsimtr01.IAction
+import cc.altruix.econsimtr01.IActionSubscriber
+import cc.altruix.econsimtr01.ch03.PropertiesFileSimParametersProvider
+import org.joda.time.DateTime
+
+/**
+ * @author Dmitri Pisarenko (dp@altruix.co)
+ * @version $Id$
+ * @since 1.0
+ */
+class FlourProduction(val simParamProv: PropertiesFileSimParametersProvider) :
+    IAction {
+    override fun timeToRun(time: DateTime): Boolean {
+        // TODO: Test this
+        // TODO: Implement this
+
+        throw UnsupportedOperationException()
+    }
+
+    override fun run(time: DateTime) {
+        // TODO: Test this
+        // TODO: Implement this
+    }
+
+    override fun notifySubscribers(time: DateTime) {
+    }
+
+    override fun subscribe(subscriber: IActionSubscriber) {
+    }
+}
