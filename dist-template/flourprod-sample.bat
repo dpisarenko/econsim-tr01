78323cc2d438e283042612d840501468076b4634
Dmitri Pisarenko <dp@altruix.co>
2016-05-22 21:50:45 +0300

    Bugfix in distribution file.

---
+++ dist-template/flourprod-sample.bat
@@ -0,0 +1,1 @@
+java -cp econsim-tr01-0.2-jar-with-dependencies.jar cc.altruix.econsimtr01.flourprod.FlourProductionSimulationAppKt flourprodRye.properties flourprodWheat.properties %3 %4 %5 %6 %7 %8 %9
